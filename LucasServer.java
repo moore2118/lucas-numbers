f3bf33ae6f5dc61c80973f3c33b149f81a1051d1
John Naylor <jonaylor89@gmail.com>
2017-05-03 01:49:32 -0400

    LNumber Object update
    Updating class to support the lucas numbers being object rather than normal integers

--- LucasServer.java
+++ LucasServer.java
@@ -8,43 +8,40 @@ import java.util.ArrayList;
 
  class LucasServer {
 
-     private static ArrayList<Integer> luci = new ArrayList<>();
-     private static ArrayList<Integer> luciOfFive = new ArrayList<>();
+     private static ArrayList<LNumber> luci = new ArrayList<>();
 
     static void lucasNumbers(int count) {
 
         luci.clear();
-        luciOfFive.clear();
 
         int x = 2, y = 1;
-        luci.add(x);
-        luci.add(y);
+        luci.add(new LNumber(x));
+        luci.add(new LNumber(y));
 
         for(int i = 2; i < count; i++){
             int z = x + y;
-            luci.add(z);
+            luci.add(new LNumber(z));
 
             x = y;
             y = z;
         }
-
-        System.out.println("The Lucas numbers up to " + count + "are: ");
-        System.out.println(luci);
     }
 
     static void checkDivisibility(){
 
-        for(int j : luci) {
-            if (j % 5 == 0) {
-                luciOfFive.add(j);
+        for(LNumber j : luci) {
+            if (j.getValue() % 5 == 0) {
+                j.setDivisible(true);
             }
         }
+    }
 
-        if(luciOfFive.size() > 0) {
-            System.out.println("Of those numbers these are divisible by 5: ");
-            System.out.println(luciOfFive);
-        }else {
-            System.out.println("None of the numbers were divisible by 5");
+    static void dump(){
+
+        System.out.println("Value : Divisible by 5");
+
+        for(LNumber l : luci) {
+            System.out.println(l.toString());
         }
     }
 
